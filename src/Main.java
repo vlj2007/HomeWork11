58725f934c4f1bc71ecdd821cf5f903aca41dd6a
vlj2007 <vlj2007@yandex.ru>
2023-08-11 19:26:18 +0700

    I have finished task2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static void showYears(int year) {
@@ -17,6 +19,30 @@ public class Main {
         System.out.println("Задача 1");
         int year = 1982;
         showYears(year);
+    }
+
+    public static void inputTypeOS(int clientOS, int clientDeviceYear) {
+        if (clientOS == 0) {
+            if (clientDeviceYear <= 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        } else if (clientOS == 1) {
+            if (clientDeviceYear <= 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        }
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        int os = 0;
+        int ageDevice = LocalDate.now().getYear();
+        inputTypeOS(os, ageDevice);
+
     }
 
 
